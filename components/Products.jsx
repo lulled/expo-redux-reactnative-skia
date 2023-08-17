80ac49d18e4a379d0ebfa390b054e144ce52d2ac
lule ahmed <ahmedlule31@gmail.com>
2023-08-17 22:01:09 +0300

    Initial commit
    Generated by create-expo-app 2.0.3.

---
+++ components/Products.jsx
@@ -0,0 +1,11 @@
+import { View, Text, FlatList } from 'react-native'
+import React from 'react'
+
+
+export default function Products() {
+  return (
+    <View>
+     <FlatList/> 
+    </View>
+  )
+}
