be28762a0f96600455d9dba34fdcb66409da145d
lule ahmed <ahmedlule31@gmail.com>
2023-08-22 03:10:36 +0300

    changed color

--- components/WellCome.jsx
+++ components/WellCome.jsx
@@ -7,7 +7,7 @@ import { useNavigation } from '@react-navigation/native'
 export default function WellCome() {
     const navigation =useNavigation();
   return (
-    <View className='flex-1 bg-teal-800'>
+    <View className='flex-1 bg-indigo-900'>
       <TouchableOpacity className=' justify-between ml-10 mt-20  mr-6 items-end' onPress={()=>navigation.navigate('Shop',{})}>
       <Text className='w-[80px] bg-white text-black text-center font-thin text-[10px] p-2 rounded-lg'>Continue</Text>
       </TouchableOpacity>
